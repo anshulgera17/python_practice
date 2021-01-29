a320072387bd7f03586a905f71be568f1f0398c6
anshulgera <anshulgera351gmail.com>
2021-01-30 01:17:08 +0530

    add book code that practiced

--- interview_practice/vowels1.py
+++ interview_practice/vowels1.py
@@ -1,10 +1,5 @@
 vowels = ['a', 'e', 'i', 'o', 'u']
-word = input("Provide a word to search for vowels: ")
-found = []
+word = "Milliways"
 for letter in word:
-    if letter in vowels:
-        if letter not in found:
-            found.append(letter)
-
-for vowel in found:
-    print(vowel)
+          if letter in vowels:
+              print(letter)
